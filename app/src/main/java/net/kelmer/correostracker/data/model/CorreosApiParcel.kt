2f21aaa2fed7300c7dd85dc15c5bccdfe4489f22
Iria Lopez <ilopezvazquez@mobiquityinc.com>
2018-03-25 13:45:35 +0200

    Data classes for API

---
+++ app/src/main/java/net/kelmer/correostracker/data/model/CorreosApiParcel.kt
@@ -0,0 +1,7 @@
+package net.kelmer.correostracker.data.model
+
+
+data class CorreosApiParcel(val codEnvio: String,
+                            val eventos: List<CorreosApiEvent>){
+    fun isDelivered(): Boolean = eventos.get(eventos.size-1).desTextoResumen.equals("Entregado")
+}
