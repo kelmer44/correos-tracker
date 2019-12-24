1f2ae3307d9b4b56bd21e3f46f9e3a8ef2f8d49a
irialopez <iria.lopez.vazquez@gmail.com>
2019-12-24 18:27:35 +0100

    Refresh from remote after coming back successfully from creating a parcel

--- app/src/main/java/net/kelmer/correostracker/ui/list/ParcelListActivity.kt
+++ app/src/main/java/net/kelmer/correostracker/ui/list/ParcelListActivity.kt
@@ -1,13 +1,11 @@
 package net.kelmer.correostracker.ui.list
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.crashlytics.android.Crashlytics
-import dagger.Module
 import kotlinx.android.synthetic.main.activity_main.*
 import net.kelmer.correostracker.R
 import net.kelmer.correostracker.base.activity.BaseActivity
-import net.kelmer.correostracker.di.activity.ActivityModule
 import net.kelmer.correostracker.ui.create.CreateActivity
 
 class ParcelListActivity : BaseActivity() {
@@ -15,14 +13,27 @@ class ParcelListActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(true)
 
         fab.setOnClickListener {
-            startActivity(CreateActivity.newIntent(this))
+            startActivityForResult(CreateActivity.newIntent(this),
+                REQ_CREATE_PARCEL)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQ_CREATE_PARCEL && resultCode == Activity.RESULT_OK){
+            for (fragment in supportFragmentManager.fragments) {
+                fragment.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
+
+    companion object {
+        const val REQ_CREATE_PARCEL = 101
     }
 }
